06b30cce14ceb88cbe111558c1914adbdbae95fb
ruihong123 <wang4996@purdue.edu>
2023-09-17 22:20:37 -0400

    LOG_DEBUG

--- scripts/benchmark-all.sh
+++ scripts/benchmark-all.sh
@@ -374,7 +374,7 @@ run_node_test() {
 # node test
 echo "**************************run node test****************************"
 result_file=$bin/results/node
-node_range="1"
+node_range="2"
 thread_range="1"
 remote_range="100" #"20 40 60 80 100"
 shared_range="0 20 50 80 100"
