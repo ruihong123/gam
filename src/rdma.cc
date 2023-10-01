b67d1415b94b86e249fdf789d8ceeee69c878be0
wang4996 <wang4996@purdue.edu>
2023-09-30 23:45:04 -0400

    DEBUG LOCAL CACHE PROBLEM

--- src/rdma.cc
+++ src/rdma.cc
@@ -636,8 +636,8 @@ ssize_t RdmaContext::Rdma(ibv_wr_opcode op, const void* src, size_t len,
     sge_list.addr = (uintptr_t) src;
     sge_list.lkey = resource->bmr->lkey;
     wr.wr.rdma.remote_addr = (uintptr_t) dest;
-//      epicLog(LOG_WARNING, "RDMA write destination is %p", dest);
-      epicAssert(dest);
+      epicLog(LOG_WARNING, "RDMA write destination is %p", dest);
+      assert(dest);
     wr.wr.rdma.rkey = rkey;
     if (op == IBV_WR_RDMA_WRITE_WITH_IMM) {
       wr.imm_data = htonl(imm);
