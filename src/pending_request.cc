1664b875245f6c1921c3948b85c4ebf7c61f4489
ruihong123 <wang4996@purdue.edu>
2023-10-09 14:11:53 -0400

    disaggreagte the GAM protocol

--- src/pending_request.cc
+++ src/pending_request.cc
@@ -587,7 +587,7 @@ void Worker::ProcessPendingInvalidateForward(Client* cli, WorkRequest* wr) {
 
     if (WRITE == parent->op) {
       lcli->Write(parent->ptr, laddr, parent->size);
-      epicLog(LOG_WARNING, "write the data (size = %ld) to destination",
+      epicLog(LOG_INFO, "write the data (size = %ld) to destination",
               parent->size);
     } else {  //WRITE_PERMISSION_ONLY
       epicAssert(WRITE_PERMISSION_ONLY == parent->op);
