300f29ed7d05794d535bb8f70f8a1fdce879476b
wang4996 <wang4996@purdue.edu>
2023-10-08 22:06:47 -0400

    DEBUG LOCAL CACHE PROBLEM

--- src/worker.cc
+++ src/worker.cc
@@ -783,7 +783,7 @@ void Worker::AddToPending(unsigned int id, WorkRequest* wr) {
   epicLog(LOG_DEBUG, "add pending work %d, wr->op = %d, wr addr = %lx", id,
       wr->op, wr);
   pending_works[id] = wr;
-  ending_works2[id] = wr;
+  pending_works2[id] = wr;
   //UNLOCK_MICRO(pending_works, id);
 }
 
