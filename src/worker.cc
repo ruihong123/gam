ebbb3db38e653f9e5d54561511deebc342d02744
ruihong123 <wang4996@purdue.edu>
2024-01-08 22:39:34 -0500

    disaggreagte the GAM protocol

--- src/worker.cc
+++ src/worker.cc
@@ -500,7 +500,10 @@ Client* Worker::GetClient(GAddr addr) {
         if (max < entry.second->GetFreeMem()) {
           max = entry.second->GetFreeMem();
           wid = entry.first;
-            assert(wid > 2);// assert the block is not allocated in the compute node
+            if(wid > 2){
+                epicLog(LOG_DEBUG, "wid is %d", wid);
+                epicAssert(false);
+            };// assert the block is not allocated in the compute node
           cli = entry.second;
         }
       }
