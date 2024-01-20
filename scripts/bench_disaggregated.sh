43c70b69b2f1f1ee5fb13f457ab324a7b870410a
ruihong123 <wang4996@purdue.edu>
2024-01-20 16:21:22 -0500

    disaggreagte the GAM protocol

--- scripts/bench_disaggregated.sh
+++ scripts/bench_disaggregated.sh
@@ -488,10 +488,10 @@ run_node_test() {
 # node test
 echo "**************************run node test****************************"
 result_file=$bin/results/node
-node_range="1 2 4 8"
+node_range="8"
 thread_range="16"
 remote_range="100" #"20 40 60 80 100"
-shared_range="0"
+shared_range="100"
 read_range="0 50 95 100"
 space_range="0"
 time_range="0"
