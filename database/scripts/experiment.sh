341fc85110bb03f4619caa5ac6468e6d67875780
ruihong123 <wang4996@purdue.edu>
2024-02-04 17:06:42 -0500

    Enable the TPC-C benchmark.

--- database/scripts/experiment.sh
+++ database/scripts/experiment.sh
@@ -22,8 +22,8 @@ memory_list=`./get_servers.sh ${Memory_file} | tr "\\n" " "`
 compute_nodes=(`echo ${compute_list}`)
 memory_nodes=(`echo ${memory_list}`)
 master_host=${compute_nodes[0]}
-cache_mem_size=8 # 8 gb Local memory size
-remote_mem_size=8 # 8 gb Remote memory size pernode is enough
+cache_mem_size=4 # 8 gb Local memory size
+remote_mem_size=4 # 8 gb Remote memory size pernode is enough
 port=$((13000+RANDOM%1000))
 
 compute_ARGS="$@"
