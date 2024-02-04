10fb7a85702f720caff649a37175eb84e2d691eb
ruihong123 <wang4996@purdue.edu>
2024-02-04 18:35:11 -0500

    Enable the TPC-C benchmark.

--- src/pending_request.cc
+++ src/pending_request.cc
@@ -127,7 +127,10 @@ void Worker::ProcessPendingRead(Client* cli, WorkRequest* wr) {
   if (--parent->counter == 0) {  //read all the data
     parent->unlock();
 #ifndef NDEBUG
-    if (parent->op == READ && parent->size > 8) {
+    if (parent->op == READ && parent->size > 24) {
+        if (parent->size > 50){
+            printf("Break point\n");
+        }
         assert(buffer_is_not_all_zero((char*)parent->ptr, parent->size));
         printf("Buffer %p content is not zero, parent->size = %zu\n", parent->ptr, parent->size);
     }
