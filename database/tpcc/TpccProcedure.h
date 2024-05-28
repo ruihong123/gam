f881e8bfcdc2336e9f04d6fe41d324aafe696aac
ruihong123 <wang4996@purdue.edu>
2024-05-28 17:53:33 -0400

    Enable the TPC-C benchmark.

--- database/tpcc/TpccProcedure.h
+++ database/tpcc/TpccProcedure.h
@@ -132,7 +132,7 @@ class NewOrderProcedure : public StoredProcedure {
   virtual ~NewOrderProcedure() {
   }
   virtual bool Execute(TxnParam *param, CharArray &ret) {
-    epicLog(LOG_DEBUG, "thread_id=%u,start new order", thread_id_);
+    epicLog(LOG_WARNING, "thread_id=%u,start new order", thread_id_);
     NewOrderParam *new_order_param = static_cast<NewOrderParam*>(param);
     double total = 0;
     //new_order_param->ol_cnt_ : number of item to be checked out.
@@ -349,7 +349,7 @@ class PaymentProcedure : public StoredProcedure {
   }
 
   virtual bool Execute(TxnParam *param, CharArray &ret) {
-    epicLog(LOG_DEBUG, "thread_id=%u,start payment", thread_id_);
+    epicLog(LOG_WARNING, "thread_id=%u,start payment", thread_id_);
     PaymentParam *payment_param = static_cast<PaymentParam*>(param);
     // "getWarehouse": "SELECT W_NAME, W_STREET_1, W_STREET_2, W_CITY, W_STATE, W_ZIP FROM WAREHOUSE WHERE W_ID = ?"
     // "updateWarehouseBalance": "UPDATE WAREHOUSE SET W_YTD = W_YTD + ? WHERE W_ID = ?"
