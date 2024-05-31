45ca59b4339501323ecce5cc0fd5755bb9e40de5
ruihong123 <wang4996@purdue.edu>
2024-05-31 00:14:21 -0400

    Enable the TPC-C benchmark.

--- database/scripts/experiment.sh
+++ database/scripts/experiment.sh
@@ -103,7 +103,7 @@ vary_temp_locality () {
 
 auto_fill_params () {
   # so that users don't need to specify parameters for themselves
-  compute_ARGS="-p$port -sf2 -sf1 -c1 -t100000 -fc../tpcc/compute.txt -fm../tpcc/memory.txt"
+  compute_ARGS="-p$port -sf2 -sf1 -c1 -t50000 -fc../tpcc/compute.txt -fm../tpcc/memory.txt"
 }
 
 auto_fill_params
