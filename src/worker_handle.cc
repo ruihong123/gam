f751478874787729bfa3c9e3a3d9cdcbe37368f7
ruihong123 <wang4996@purdue.edu>
2024-06-02 01:52:55 -0400

    Enable the TPC-C benchmark.

--- src/worker_handle.cc
+++ src/worker_handle.cc
@@ -158,7 +158,7 @@ int WorkerHandle::SendRequest(WorkRequest* wr) {
 #endif
         int cnt = 0;
         while (*local_notify_buf != 2){
-            spin_wait_ns(1000);
+            spin_wait_ns(5000);
             if (cnt++ > 100000 && wr->op == WLOCK && populate_end){
                 delete_mr = false;
                 break;
